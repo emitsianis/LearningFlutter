579e4a3fa508f557f29a9b4f1a669e01139e4d9d
Vangelis Mitsianis <ev.mitsianis@gmail.com>
2023-12-10 21:32:17 +0200

    Add: Categories and Recommendations

---
+++ lib/models/diet_model.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+class DietModel {
+  String name;
+  String iconPath;
+  String level;
+  String duration;
+  String calorie;
+  bool viewIsSelected;
+  Color boxColor;
+
+  DietModel({
+    required this.name,
+    required this.iconPath,
+    required this.level,
+    required this.duration,
+    required this.calorie,
+    required this.viewIsSelected,
+    required this.boxColor,
+  });
+
+  static List<DietModel> getDiets() {
+    List<DietModel> diets = [];
+
+    diets.add(
+      DietModel(
+        name: 'Honey Pancake',
+        iconPath: 'assets/icons/honey-pancakes.svg',
+        level: 'Easy',
+        duration: '30min',
+        calorie: '180kcal',
+        viewIsSelected: true,
+        boxColor: const Color(0xff92A3FD),
+      ),
+    );
+
+    diets.add(
+      DietModel(
+        name: 'Canai Bread',
+        iconPath: 'assets/icons/canai-bread.svg',
+        level: 'Easy',
+        duration: '20min',
+        calorie: '230kcal',
+        viewIsSelected: false,
+        boxColor: const Color(0xffC58BF2),
+      ),
+    );
+
+    return diets;
+  }
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,78 +1,255 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:test_project/models/diet_model.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+import '../models/category_model.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<CategoryModel> categories = [];
+  List<DietModel> diets = [];
+
+  @override
+  void initState() {
+    super.initState();
+    categories = CategoryModel.getCategories();
+    diets = DietModel.getDiets();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
       backgroundColor: Colors.white,
-      body: Column(children: [
-        buildContainer(),
+      body: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        buildSearcInput(),
+        const SizedBox(height: 40),
+        buildCategories(),
+        const SizedBox(height: 40),
+        buildRecommendations(),
       ]),
     );
   }
 
-  Container buildContainer() {
+  Column buildRecommendations() {
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: Text(
+              'Recommendation\nFor Diet',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          const SizedBox(height: 15),
+          Container(
+            height: 240,
+            child: ListView.separated(
+              itemBuilder: (context, index) {
+                return Container(
+                  width: 210,
+                  decoration: BoxDecoration(
+                    color: diets[index].boxColor.withOpacity(0.3),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      SvgPicture.asset(diets[index].iconPath),
+                      Column(
+                        children: [
+                          Text(
+                            diets[index].name,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w500,
+                              color: Colors.black,
+                              fontSize: 16,
+                            ),
+                          ),
+                          Text(
+                            '${diets[index].level} | ${diets[index].duration} | ${diets[index].calorie}',
+                            style: const TextStyle(
+                                fontWeight: FontWeight.w400,
+                                fontSize: 13,
+                                color: Color(0xff7B6F72)),
+                          ),
+                        ],
+                      ),
+                      Container(
+                          height: 45,
+                          width: 130,
+                          decoration: BoxDecoration(
+                              gradient: LinearGradient(
+                                colors: [
+                                  diets[index].viewIsSelected
+                                      ? const Color(0xff9DCEFF)
+                                      : Colors.transparent,
+                                  diets[index].viewIsSelected
+                                      ? const Color(0xff92A3FD)
+                                      : Colors.transparent,
+                                ],
+                              ),
+                              borderRadius: BorderRadius.circular(50)),
+                          child: Center(
+                            child: Text(
+                              'View',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w600,
+                                color: diets[index].viewIsSelected
+                                    ? Colors.white
+                                    : const Color(0xffC58BF2),
+                                fontSize: 14,
+                              ),
+                            ),
+                          )),
+                    ],
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(width: 15);
+              },
+              itemCount: diets.length,
+              scrollDirection: Axis.horizontal,
+              padding: const EdgeInsets.only(left: 20, right: 20),
+            ),
+          )
+        ],
+      );
+  }
+
+  Column buildCategories() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.only(left: 20),
+          child: Text(
+            'Category',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ),
+        const SizedBox(height: 15),
+        Container(
+          height: 120,
+          child: ListView.separated(
+            itemCount: categories.length,
+            scrollDirection: Axis.horizontal,
+            separatorBuilder: (context, index) {
+              return const SizedBox(width: 25);
+            },
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            itemBuilder: (context, index) {
+              return Container(
+                width: 100,
+                decoration: BoxDecoration(
+                  color: categories[index].boxColor.withOpacity(0.3),
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      width: 50,
+                      height: 50,
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                      ),
+                      child: Padding(
+                          padding: const EdgeInsets.all(8),
+                          child: SvgPicture.asset(categories[index].iconPath)),
+                    ),
+                    Text(
+                      categories[index].name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+
+  Container buildSearcInput() {
     return Container(
-      margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+      margin: const EdgeInsets.only(top: 40, left: 20, right: 20),
       decoration: BoxDecoration(
         boxShadow: [
           BoxShadow(
-              color: Color(0xff1D1617).withOpacity(0.11),
+              color: const Color(0xff1D1617).withOpacity(0.11),
               blurRadius: 40,
-              spreadRadius: 0
-          ),
+              spreadRadius: 0),
         ],
       ),
       child: TextField(
         decoration: InputDecoration(
-            filled: true,
-            fillColor: Colors.white,
-            contentPadding: EdgeInsets.all(15),
-            hintText: 'Search Pancake',
-            hintStyle: TextStyle(
-              color: Color(0xffDDDADA),
-              fontSize: 14,
-            ),
-            prefixIcon: Padding(
-              padding: const EdgeInsets.all(12),
-              child: SvgPicture.asset('assets/icons/Search.svg'),
-            ),
-            suffixIcon: Container(
-              width: 100,
-              child: IntrinsicHeight(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    VerticalDivider(
-                      color: Colors.black,
-                      indent: 10,
-                      endIndent: 10,
-                      thickness: 0.1,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(12),
-                      child: SvgPicture.asset('assets/icons/Filter.svg'),
-                    ),
-                  ],
-                ),
+          filled: true,
+          fillColor: Colors.white,
+          contentPadding: EdgeInsets.all(15),
+          hintText: 'Search Pancake',
+          hintStyle: const TextStyle(
+            color: Color(0xffDDDADA),
+            fontSize: 14,
+          ),
+          prefixIcon: Padding(
+            padding: const EdgeInsets.all(12),
+            child: SvgPicture.asset('assets/icons/Search.svg'),
+          ),
+          suffixIcon: Container(
+            width: 100,
+            child: IntrinsicHeight(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const VerticalDivider(
+                    color: Colors.black,
+                    indent: 10,
+                    endIndent: 10,
+                    thickness: 0.1,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: SvgPicture.asset('assets/icons/Filter.svg'),
+                  ),
+                ],
               ),
             ),
-            border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(15),
-        borderSide: BorderSide.none,
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15),
+            borderSide: BorderSide.none,
+          ),
+        ),
       ),
-    ),)
-    ,
     );
   }
 
   AppBar buildAppBar() {
     return AppBar(
-      title: Text(
+      title: const Text(
         'Breakfast',
         style: TextStyle(
           color: Colors.black,
@@ -86,9 +263,9 @@ class HomePage extends StatelessWidget {
       leading: GestureDetector(
         onTap: () {},
         child: Container(
-          margin: EdgeInsets.all(10),
+          margin: const EdgeInsets.all(10),
           decoration: BoxDecoration(
-            color: Color(0xffF7F8F8),
+            color: const Color(0xffF7F8F8),
             borderRadius: BorderRadius.circular(10),
           ),
           alignment: Alignment.center,
@@ -101,9 +278,9 @@ class HomePage extends StatelessWidget {
       ),
       actions: [
         Container(
-          margin: EdgeInsets.all(10),
+          margin: const EdgeInsets.all(10),
           decoration: BoxDecoration(
-            color: Color(0xffF7F8F8),
+            color: const Color(0xffF7F8F8),
             borderRadius: BorderRadius.circular(10),
           ),
           alignment: Alignment.center,
